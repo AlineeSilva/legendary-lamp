88dd55d19c47d37302b819b5fe5a8d8b0c4be9de
Robton Brangaitis <robtonbrangaitis@outlook.com>
2017-09-08 15:54:48 -0300

    Alteração de Exceções e apresentação de mensagem de erro

--- Somatorio/src/main/java/s2b_teste/Somatorio/Soma.java
+++ Somatorio/src/main/java/s2b_teste/Somatorio/Soma.java
@@ -4,9 +4,10 @@ public class Soma {
 
 	public static double soma(double d, double e) {
 		if(d < 0) {
-			throw new IllegalArgumentException();
-		}else if(e < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O primeiro número não pode ser negativo");
+		}
+		if(e < 0){
+			throw new IllegalArgumentException("O segundo número não pode ser negativo");
 		}
 		return d + e;
 	}
